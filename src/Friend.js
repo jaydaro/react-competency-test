1a700a04f5f9f44c7f1af2a15119608a43579aec
stephlane544 <stephlane544@gmail.com>
2019-07-31 11:40:59 -0600

    displayed friends data on page

--- src/Friend.js
+++ src/Friend.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
 class Friends extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
     render() { 
+        let {first_name, last_name, img, status} = this.props.friend;
         return (
-            <div>
-                
+            <div className='friend'>
+                <img className='overviewImg' src={img} alt=""/>
+                <div className='nameAndStatus'>
+                    <div className='fullName'>{first_name} {last_name}</div>
+                    <div className='status'>{status}</div>
+                </div>
+                <button onClick={this.props.showDetails}className='showDetsButton'>Select</button>
             </div>
         );
     }

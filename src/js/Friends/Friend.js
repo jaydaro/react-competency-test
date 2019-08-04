beb45c4668d0485b5843eb78e09c96ce5415b906
Liz Lyon <elizabethlainlyon@gmail.com>
2019-08-04 14:36:54 -0600

    main branch

---
+++ src/js/Friends/Friend.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { shape } from 'prop-types';
+
+export default function Friend(person) {
+    return (
+        <div className="person-tile">
+        {console.log('person', person)}
+            <img className="person-image" src={person.img}/>
+            <div className="person-text">
+                <div className="person-details">
+                    <div className="person-fullname">{person.first_name} {person.last_name}</div>
+                    <div className="person-status">{person.status}</div>
+                </div>
+                <button>Select</button>
+            </div>
+        </div>
+    );
+}
+
+Friend.propTypes = {
+    person: shape().isRequired
+}
---
+++ src/js/Friends/FriendsList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Friend from './Friend';
+
+export default class FriendsList extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            friendsList: {}
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://private-5bdb3-friendmock.apiary-mock.com/friends')
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                const jsonPayload = json;
+                this.setState({ friendsList: jsonPayload });
+            });
+    }
+
+    createList() {
+        const data = this.state.friendsList;
+        if (!data[0]) return;
+        return data.map(person => {
+            console.log(person.first_name);
+            return (
+                <Friend
+                    {...person}
+                />
+            )
+        });
+    }
+
+    render() {
+        return (
+            <div className="person-list">
+                {this.createList()}
+            </div>
+        )
+    }
+}

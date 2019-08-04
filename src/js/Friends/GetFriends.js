a520cd3fa1da18714fba29a145b32021b3fc0a91
Liz Lyon <elizabethlainlyon@gmail.com>
2019-08-04 14:40:42 -0600

    remove unused files

--- src/js/Friends/GetFriends.js
+++
@@ -1,9 +0,0 @@
-export const getFriends = fetch('http://private-5bdb3-friendmock.apiary-mock.com/friends')
-.then(response => {
-    return response.json();
-  })
- .then(json =>  {
-    const jsonPayload = JSON.stringify(json);
-    console.log('hello from getFriends', jsonPayload);
-    return jsonPayload;
-  });
